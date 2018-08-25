846269ef2f50939032bbdf818b26c619ed653b47
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-08-25 02:07:35 -0700

    last output, test UDHR

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 
 import codecs
 import zg2uni
+import uni2zg
+import win2uni
+import uni2win
 import sys
 
 input_file_name = sys.argv[1]
@@ -14,6 +17,33 @@ output_file = codecs.open(output_file_name, encoding='utf-8', mode='w')
 
 for input_line in input_file:
     input_line = zg2uni.convert(input_line)
+    output_file.write(input_line)
+    output_file.flush()
+
+for input_line in input_file:
+    input_line = uni2zg.convert(input_line)
+    output_file.write(input_line)
+    output_file.flush()
+
+for input_line in input_file:
+    input_line = win2uni.convert(input_line)
+    output_file.write(input_line)
+    output_file.flush()
+
+for input_line in input_file:
+    input_line = uni2win.convert(input_line)
+    output_file.write(input_line)
+    output_file.flush()
+
+for input_line in input_file:
+    input_line = zg2uni.convert(input_line)
+    input_line = uni2win.convert(input_line)
+    output_file.write(input_line)
+    output_file.flush()
+
+for input_line in input_file:
+    input_line = win2uni.convert(input_line)
+    input_line = uni2zg.convert(input_line)
     output_file.write(input_line)
     output_file.flush()
 
